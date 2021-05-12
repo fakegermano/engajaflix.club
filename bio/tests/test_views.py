3623294d9281b8fa623eaa5253f6bff83f17d230
daniel <danielgtravieso@gmail.com>
2021-05-11 21:22:52 -0300

    added more tests to some auth views

--- bio/tests/test_views.py
+++ bio/tests/test_views.py
@@ -21,3 +21,61 @@ class TestHomeView:
         assert username in str(response.content)
 
 
+class TestLoginView:
+    def test_anonymous(self):
+        c = Client()
+        response = c.get('/login/')
+        assert response.status_code == 200
+        assert '</form>' in str(response.content)
+
+    def test_logged_in(self):
+        c = Client()
+        user = mixer.blend(AUTH_USER_MODEL)
+        c.force_login(user)
+        response = c.get('/login/')
+        assert response.status_code == 200
+        assert 'logout' in str(response.content)
+
+
+class TestLogoutView:
+    def test_anonymous(self):
+        c = Client()
+        response = c.get('/logout/')
+        assert response.status_code == 200
+
+    def test_logged_in(self):
+        c = Client()
+        user = mixer.blend(AUTH_USER_MODEL)
+        c.force_login(user)
+        response = c.get('/logout/')
+        assert response.status_code == 200
+
+
+class TestPasswordResetView:
+    def test_anonymous(self):
+        c = Client()
+        response = c.get('/password/reset/')
+        assert response.status_code == 200
+        assert '</form>' in str(response.content)
+
+    def test_logged_in(self):
+        c = Client()
+        user = mixer.blend(AUTH_USER_MODEL)
+        c.force_login(user)
+        response = c.get('/password/reset/')
+        assert response.status_code == 200
+        assert '</form>' in str(response.content)
+
+
+class TestPasswordChangeView:
+    def test_anonymous(self):
+        c = Client()
+        response = c.get('/password/change/')
+        assert 'login' in response.url
+
+    def test_logged_in(self):
+        c = Client()
+        user = mixer.blend(AUTH_USER_MODEL)
+        c.force_login(user)
+        response = c.get('/password/change/')
+        assert response.status_code == 200
