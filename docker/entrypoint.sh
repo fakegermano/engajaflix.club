540edaf998f5048d49954afb7f7d893437720fa7
fakegermano <danielgtravieso@gmail.com>
2023-07-12 22:16:41 +0200

    changes to deploy flow

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-tailwindcss -i /static/css/style.css -o /static/css/output.css --minify
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py makesuperuser
-gunicorn config.wsgi --config="/code/docker/gunicorn.conf.py"
